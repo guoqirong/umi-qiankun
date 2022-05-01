92148b35ae6b97990e1e0f8741da58256cd3130a
guoqirong <2834964850@qq.com>
2022-05-01 22:13:59 +0800

    添加左侧菜单切换主应用及子应用，及打包分包zip压缩处理

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,7 @@
 import { routes } from './src/router';
 import { defineConfig } from 'umi';
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const prodGzipList = ['js', 'css'];
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -9,6 +11,13 @@ export default defineConfig({
   history: {
     type: 'hash',
   },
+  copy: [
+    {
+      from: './mock/config.json',
+      to: 'config.json',
+    },
+  ],
+  publicPath: process.env.NODE_ENV === 'dev' ? '/umi-qiankun/' : '/',
   qiankun: {
     master: {
       // 注册子应用信息
@@ -21,4 +30,41 @@ export default defineConfig({
     },
   },
   fastRefresh: {},
+  chainWebpack(config) {
+    if (process.env.UMI_ENV !== 'dev') {
+      config.merge({
+        optimization: {
+          minimize: true,
+          splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+              vendors: {
+                name: 'vendors',
+                chunks: 'all',
+                test: /[\\/]node_modules[\\/](react|react-dom|events)[\\/]/,
+                priority: -10,
+              },
+            },
+          },
+        },
+      });
+      config.plugin('compression-webpack-plugin').use(
+        new CompressionWebpackPlugin({
+          // filename: 文件名称，这里我们不设置，让它保持和未压缩的文件同一个名称
+          algorithm: 'gzip', // 指定生成gzip格式
+          test: new RegExp('\\.(' + prodGzipList.join('|') + ')$'), // 匹配哪些格式文件需要压缩
+          threshold: 10240, //对超过10k的数据进行压缩
+          minRatio: 0.6, // 压缩比例，值为0 ~ 1
+        }),
+      );
+    }
+  },
+  chunks: process.env.UMI_ENV !== 'dev' ? ['vendors', 'umi'] : ['umi'],
 });
