df7153ec76db09762263f99e8baf3fe38a49f885
guoqirong <2834964850@qq.com>
2022-05-02 15:15:19 +0800

    添加umi子系统

--- mock/config.ts
+++ mock/config.ts
@@ -6,21 +6,21 @@ export default {
         entry: 'https://guoqirong.github.io/vue3-cnode/',
         activeRule: '/#/vue3-cnode', // hash路由需配置
       },
-      // {
-      //   name: 'umi-cnode',
-      //   entry: 'http://localhost/vue3-cnode/',
-      //   activeRule: '/#/vue3-cnode', // hash路由需配置
-      // },
+      {
+        name: 'umi-cnode',
+        entry: 'https://guoqirong.github.io/umi-cnode/',
+        activeRule: '/#/umi-cnode', // hash路由需配置
+      },
     ],
     routes: [
       {
         path: '/vue3-cnode',
         microApp: 'vue3-cnode',
       },
-      // {
-      //   path: '/umi-cnode',
-      //   microApp: 'umi-cnode',
-      // },
+      {
+        path: '/umi-cnode',
+        microApp: 'umi-cnode',
+      },
     ],
   },
 };
